aa8298900e8c527159e08f49fb76fa7e67c1f36b
Brian Martin <brayniac@gmail.com>
2016-10-29 17:12:13 +0000

    handle ArpRx differently for requests and replies

--- src/arp/arp_rx.rs
+++ src/arp/arp_rx.rs
@@ -3,6 +3,7 @@ use std::time::SystemTime;
 
 use pnet::packet::ethernet::{EtherType, EtherTypes, EthernetPacket};
 use pnet::packet::arp::ArpPacket;
+use pnet::packet::arp::ArpOperations::*;
 use pnet::packet::Packet;
 
 use {RxError, RxResult, VersionedTx};
@@ -27,25 +28,39 @@ impl ArpRx {
     }
 }
 
+#[allow(non_upper_case_globals)]
 impl EthernetListener for ArpRx {
     fn recv(&mut self, _time: SystemTime, pkg: &EthernetPacket) -> RxResult {
         let arp_pkg = ArpPacket::new(pkg.payload()).unwrap();
-        let ip = arp_pkg.get_sender_proto_addr();
-        let mac = arp_pkg.get_sender_hw_addr();
 
-        let mut data = try!(self.data.lock().or(Err(RxError::PoisonedLock)));
-        let old_mac = data.table.insert(ip, mac);
-        if old_mac.is_none() || old_mac != Some(mac) {
-            // The new MAC is different from the old one, bump tx VersionedTx
-            debug!("ARP, Learned {} is-at {}", ip, mac);
-            try!(self.vtx.lock().or(Err(RxError::PoisonedLock))).inc();
-        }
-        if let Some(listeners) = data.listeners.remove(&ip) {
-            for listener in listeners {
-                debug!("ARP, Reply {} is-at {}", ip, mac);
-                listener.send(mac).unwrap_or(());
+
+        match arp_pkg.get_operation() {
+            Request => {
+                let tgt_ip = arp_pkg.get_target_proto_addr();
+                let src_ip = arp_pkg.get_sender_proto_addr();
+                debug!("ARP, Request who-has {} tell {}", tgt_ip, src_ip);
             }
+            Reply => {
+                let ip = arp_pkg.get_sender_proto_addr();
+                let mac = arp_pkg.get_sender_hw_addr();
+
+                let mut data = try!(self.data.lock().or(Err(RxError::PoisonedLock)));
+                let old_mac = data.table.insert(ip, mac);
+                if old_mac.is_none() || old_mac != Some(mac) {
+                    // The new MAC is different from the old one, bump tx VersionedTx
+                    debug!("ARP, Learned {} is-at {}", ip, mac);
+                    try!(self.vtx.lock().or(Err(RxError::PoisonedLock))).inc();
+                }
+                if let Some(listeners) = data.listeners.remove(&ip) {
+                    for listener in listeners {
+                        listener.send(mac).unwrap_or(());
+                    }
+                }
+            }
+            _ => {}
         }
+
+        
         Ok(())
     }
 
